3a3298ccc3721d6c41b77f82cb49887e2afc47ec
Kedar Iyer <kedarmail@gmail.com>
2019-01-08 18:06:03 +0000

    quick improvements to net-usage-blocks

--- net-usage-blocks.sh
+++ net-usage-blocks.sh
@@ -1,8 +1,8 @@
 START_BLOCK=30000000
-COUNT=100
+COUNT=28800
 END_BLOCK=$(echo "$START_BLOCK + $COUNT" | bc)
 
 for BLOCK in $(seq $START_BLOCK $END_BLOCK); do
-    NET_USAGE=$(cleos -u 'http://mainnet.libertyblock.io:8888' get block $BLOCK | jq '.transactions[] | .net_usage_words' | paste -sd+ | bc)
-    echo $NET_USAGE
+    NET_USAGE=$(cleos get block $BLOCK | jq '.transactions[] | .net_usage_words' | paste -sd+ | bc)
+    echo $NET_USAGE >> "net-$START_BLOCK-$END_BLOCK"
 done
