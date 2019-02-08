c33cf8406c2058c7c7a2b263f3635a48d01b0a5c
Ubuntu <eos@kylin.vkz3o3z3kakupcojd2rucwf1lh.lx.internal.cloudapp.net>
2019-02-08 18:21:17 +0000

    get top 30 producers

---
+++ top30prods.sh
@@ -0,0 +1,7 @@
+# get a list of active producers
+PRODUCERS=$(cleos system listproducers -l 30 | tail -n +2 | head -n 30 | awk '{print $1}')
+for prod in $PRODUCERS; do
+    echo "{ \"actor\": \"$prod\", \"permission\": \"active\" }" > $prod.perm.tmp
+done
+cat *.perm.tmp | jq -s '' > producers.json
+rm *.perm.tmp
